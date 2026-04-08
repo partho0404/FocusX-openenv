c3f701fc91c03d17fdd5190227f3365e71c065fa
Partho <parthomaji10@gmail.com>
2026-04-09 04:03:30 +0530

    Update task.py

--- FocusX-openenv/task.py
+++ FocusX-openenv/task.py
@@ -1,20 +1,22 @@
 from grader import grade_focus
 
+TASKS = [
+    {
+        "id": "task1",
+        "name": "focus_task_1",
+        "grader": "grade_focus"
+    },
+    {
+        "id": "task2",
+        "name": "focus_task_2",
+        "grader": "grade_focus"
+    },
+    {
+        "id": "task3",
+        "name": "focus_task_3",
+        "grader": "grade_focus"
+    }
+]
+
 def get_tasks():
-    return [
-        {
-            "id": "task1",
-            "name": "focus_task_1",
-            "grader": grade_focus
-        },
-        {
-            "id": "task2",
-            "name": "focus_task_2",
-            "grader": grade_focus
-        },
-        {
-            "id": "task3",
-            "name": "focus_task_3",
-            "grader": grade_focus
-        }
-    ]
+    return TASKS
